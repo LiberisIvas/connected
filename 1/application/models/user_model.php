bb7c21b8cc0fbee8b910063f9e62abd0f2680b7b
Mahmoud <m4m205@users.noreply.github.com>
2017-08-29 14:12:47 +0200

    some model changes

--- 1/application/models/user_model.php
+++ 1/application/models/user_model.php
@@ -13,8 +13,8 @@ class user_model extends CI_Model {
     public function add_user($values)
     {
         $query = "INSERT INTO users 
-        	(first_name, last_name, email, password , created_at, updated_at)
-        		VALUES (?,?,?,?,?,?)";
+        	(first_name, last_name, email, password , category , looking_for , link , story , image , created_at, updated_at)
+        		VALUES (?,?,?,?,?,?,?,?,?,?,?)";
         
              return $this->db->query($query, $values);
     }
@@ -25,17 +25,21 @@ class user_model extends CI_Model {
 
     }
 
-    public function show_post (){
+    
 
-
-    }
-
-    public function add_post($new_post){
+    public function add_message($new_post){
             $query = "INSERT INTO posts
             (users_idusers, post)
                 VALUES (?,?)";
             return $this->db->query($query, $new_post);
     }
+
+    public function show_category($check){
+        
+        return $this->db->query( "SELECT * FROM users WHERE category=?",array($check))->result_array();
+
+    }
+
 
 
 
--- 1/application/views/messages.php
+++ 1/application/views/messages.php
@@ -1,1 +1,19 @@
-<h2>messages</h2>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div>
+	
+</div>
+
+<div>
+	
+	
+</div>
+
+
+</body>
+</html>
