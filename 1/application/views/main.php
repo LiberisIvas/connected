36ddf61e31cd00408fe9e51ff1c3a487497accfb
Mikaa010 <Mikaa010@users.noreply.github.com>
2017-08-29 14:20:23 +0200

    add the slideshow need to fix images

--- 1/application/views/main.php
+++ 1/application/views/main.php
@@ -59,7 +59,30 @@ var_dump($this->session->all_userdata());
 <h2> where you connect and make your projects real</h2>
 
 
+<h2 class="w3-center">Automatic Slideshow</h2>
 
+<div class="w3-content w3-section" style="max-width:500px">
+  <img class="mySlides" src="img_la.jpg" style="width:100%">
+  <img class="mySlides" src="img_ny.jpg" style="width:100%">
+  <img class="mySlides" src="img_chicago.jpg" style="width:100%">
+</div>
+
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 4 seconds
+}
+</script>
 
 
 
