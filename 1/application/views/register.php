791f8fb1159947989537f88786ac5d9f0cd0953b
LiberisIvas <liberis.ivas@gmail.com>
2017-08-29 13:29:35 +0200

    registration changed

--- 1/application/views/register.php
+++ 1/application/views/register.php
@@ -1,6 +1,6 @@
 <h1>Register Form</h1>
 
-<div class="group">
+<<!-- div class="group">
 	<h3 class="text">* Register *</h3>
 	<h3 class="text"> Please enter your information </h3><hr width="70%"><br>
 	<form action="Login/check_register" method="POST">
@@ -29,4 +29,66 @@
 	</form>
 	
 
-</div>
+</div> -->
+
+<!-- BOOTSTRAP -->
+
+<div class="container">
+    <h1 class="well">Registration Form</h1>
+	<div class="col-lg-12 well">
+	<div class="row">
+				<form action="Login/check_register" method="POST">
+					<div class="col-sm-12">
+						<div class="row">
+							<div class="col-sm-6 form-group">
+								<label for="first_name">First Name</label>
+								<input type="text" placeholder="Enter First Name Here.." class="form-control">
+							</div>
+							<div class="col-sm-6 form-group">
+								<label for="last_name">Last Name</label>
+								<input type="text" placeholder="Enter Last Name Here.." class="form-control">
+							</div>
+								<div class="form-group">
+									<label>Email Address</label>
+									<input type="text" placeholder="Enter Email Address Here.." class="form-control">
+								</div>
+							<div class="col-sm-6 form-group">
+								<label for="password">Password</label>
+								<input type="password" placeholder="Enter password Here.." class="form-control">
+							</div>
+							<div class="col-sm-6 form-group">
+								<label for="confirm_password">Password</label>
+								<input type="password" placeholder="Enter password Here.." class="form-control">
+							</div>
+						</div>		
+						<div class="col-sm-6 form-group">			
+						<select name="category" class="form-control">
+							<option value="heads">Heads</option>
+							<option value="hands">Hands</option>
+							<option value="eyes">Eyes</option>
+							<option value="ears">Ears</option>
+							<option value="body">Body</option>
+							<option value="others">Others</option>
+							</div>
+						<div class="row">
+							<div class="col-sm-4 form-group">
+								<label for="look_for">Looking for</label>
+								<input type="text" placeholder="Enter what you are looking for.." class="form-control">
+							</div>	
+							<div class="col-sm-4 form-group">
+								<label for="link">link</label>
+								<input type="text" placeholder="Place your portfolio link" class="form-control">
+							</div>	
+						</div>						
+							
+						
+					<div class="form-group">
+						<label for="story">Story</label>
+						<input type="text" placeholder="Enter your story here.." class="form-control">
+					</div>
+					<button type="button" class="btn btn-lg btn-info">Submit</button>					
+					</div>
+				</form> 
+				</div>
+	</div>
+	</div>
