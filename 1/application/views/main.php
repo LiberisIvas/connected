f72197affde234760906045106abaf1d554b3654
Mahmoud <m4m205@users.noreply.github.com>
2017-08-29 11:25:51 +0200

    make form in html
    make form in html

--- 1/application/views/main.php
+++ 1/application/views/main.php
@@ -55,6 +55,12 @@ var_dump($this->session->all_userdata());
 		<p><input type="password" name="password" placeholder="Password"></p>
 		<p><input type="password" name="confirm_password" placeholder="Confirm password"></p>
 		<br>
+		<p><select  name="category" ></p>
+		<p><input type="text" name="look_for" placeholder="Looking for"></p>
+		<p><input type="text" name="link" placeholder="Past your profile link"></p>
+		<p><input type="text" name="story" placeholder="write your story"></p>
+		<!-- <p><input type="upload" name="image" ></p>  incase we did it -->
+
 		<input type="hidden" name="submit" value="register">
 		<input class="btn_register" type="submit" name="action" value="register">
 
