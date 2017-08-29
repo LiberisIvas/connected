d2b961b0bf8f176bf00d1ac07d71a3617f859778
Mahmoud <m4m205@users.noreply.github.com>
2017-08-29 16:51:32 +0200

    start branch

--- 1/application/controllers/Logged.php
+++ 1/application/controllers/Logged.php
@@ -6,10 +6,13 @@ class Logged extends CI_Controller{
 
 		if ($this->input->post('action')) {
 			
-			
+			$chosen= $this->input->post('action');
+			$this->load->model("user_model");
+			$info['all_info']=$this->user_model->show_category($chosen);
+			$this->load->view("user_by_category",$info);
 		}
 		else {
-			$this->load->view('users_by_category');
+			$this->load->view('logged_in');
 				
 		}
 
--- 1/application/views/messages.php
+++ 1/application/views/messages.php
@@ -1,1 +1,13 @@
 <h2 class="welcome">Welcome, php tag</h2>
+
+<div> <!-- inbox -->
+	<?php 
+ 	
+	echo ;
+
+	 ?>
+</div>
+
+<div> <!-- outbox -->
+	
+</div>
