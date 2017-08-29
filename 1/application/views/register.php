9fb673497cd1707960455e91c5efc8507778b3b9
LiberisIvas <liberis.ivas@gmail.com>
2017-08-29 13:43:13 +0200

    new names

--- 1/application/views/register.php
+++ 1/application/views/register.php
@@ -41,24 +41,24 @@
 					<div class="col-sm-12">
 						<div class="row">
 							<div class="col-sm-6 form-group">
-								<label for="first_name">First Name</label>
-								<input type="text" placeholder="Enter First Name Here.." class="form-control">
+								<label>First Name</label>
+								<input type="text" name="first_name" placeholder="Enter First Name Here.." class="form-control">
 							</div>
 							<div class="col-sm-6 form-group">
-								<label for="last_name">Last Name</label>
-								<input type="text" placeholder="Enter Last Name Here.." class="form-control">
+								<label >Last Name</label>
+								<input type="text" name="last_name" placeholder="Enter Last Name Here.." class="form-control">
 							</div>
 								<div class="form-group">
 									<label>Email Address</label>
-									<input type="text" placeholder="Enter Email Address Here.." class="form-control">
+									<input type="text" name="email" placeholder="Enter Email Address Here.." class="form-control">
 								</div>
 							<div class="col-sm-6 form-group">
-								<label for="password">Password</label>
-								<input type="password" placeholder="Enter password Here.." class="form-control">
+								<label>Password</label>
+								<input type="password" name="password" placeholder="Enter password Here.." class="form-control">
 							</div>
 							<div class="col-sm-6 form-group">
-								<label for="confirm_password">Password</label>
-								<input type="password" placeholder="Enter password Here.." class="form-control">
+								<label>Password</label>
+								<input type="password" name="confirm_password" placeholder="Enter password Here.." class="form-control">
 							</div>
 						</div>		
 						<div class="col-sm-6 form-group">			
@@ -72,19 +72,19 @@
 							</div>
 						<div class="row">
 							<div class="col-sm-4 form-group">
-								<label for="look_for">Looking for</label>
-								<input type="text" placeholder="Enter what you are looking for.." class="form-control">
+								<label>Looking for</label>
+								<input type="text" name="look_for" placeholder="Enter what you are looking for.." class="form-control">
 							</div>	
 							<div class="col-sm-4 form-group">
-								<label for="link">link</label>
-								<input type="text" placeholder="Place your portfolio link" class="form-control">
+								<label>link</label>
+								<input type="text" name="link" placeholder="Place your portfolio link" class="form-control">
 							</div>	
 						</div>						
 							
 						
 					<div class="form-group">
-						<label for="story">Story</label>
-						<input type="text" placeholder="Enter your story here.." class="form-control">
+						<label>Story</label>
+						<input type="text" name="story" placeholder="Enter your story here.." class="form-control">
 					</div>
 					<input class="btn_register" type="submit" name="action" value="register">				
 					</div>
