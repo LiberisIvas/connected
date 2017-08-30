090a79eef2861555f37bd655267586b602fc15d5
LiberisIvas <liberis.ivas@gmail.com>
2017-08-30 18:22:46 +0200

    front

--- 1/application/views/Template/header.php
+++ 1/application/views/Template/header.php
@@ -7,6 +7,7 @@
 
 
     <link rel="stylesheet" type="text/css" href="Assets\css\bootstrap.css"><link rel="stylesheet" type="text/css" href="Assets/css/style.css">  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> <link rel="stylesheet" type="text/css" href="netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     
 
   </head>
