1530a5f95669f384463fcd6c044bdf880a765be3
Mikaa010 <Mikaa010@users.noreply.github.com>
2017-08-31 13:45:25 +0200

    did styling and put pictures in buttons

--- 1/application/views/logged_in.php
+++ 1/application/views/logged_in.php
@@ -8,10 +8,9 @@
 <h2 class="welcome">Welcome <?= $this->session->userdata('loggedin_user')["first_name"]  ?>,<br></h2>
 <h3> Please select a category bellow to see users who are placed in the selected category </h3>
 
-
 <form id="head" action="Logged" method="Post">
 	
-	<input class="btn-success btn-lg" type="submit" name="act" value="Head" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit"  name="act" value="Head" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Head">
 
 <!-- <form name="action" method="Post"> -->
@@ -19,12 +18,12 @@
 <form id="hand" action="Logged" method="Post">
 
 
-	<input class="btn-success btn-lg" type="submit" name="act" value="Hand" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit" name="act" value="Hand" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Hand">
 </form>
 <form id="ear" action="Logged" method="Post">
 	
-	<input class="btn-success btn-lg" type="submit" name="act" value="Ear" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit" name="act" value="Ear" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Ear">
 </form>	
 
@@ -34,13 +33,13 @@
 
 <form id="eye" action="Logged" method="Post">
 	
-	<input class="btn-success btn-lg" type="submit" name="act" value="Eye" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit" name="act" value="Eye" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Eye">
 <!-- <form name="action" method="Post"> -->
 </form>
 <form id="body" action="Logged" method="Post">
 	
-	<input class="btn-success btn-lg" type="submit" name="act" value="Body" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit" name="act" value="Body" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Body">
 </form>
 <!-- <form name="action" method="Post"> -->
@@ -48,11 +47,11 @@
 <form id="other" action="Logged" method="Post">
 
 	
-	<input class="btn-success btn-lg" type="submit" name="act" value="Other" style="height:180px; width:33%">
+	<input class="btn-primary btn-lg" type="submit" name="act" value="Other" style="height:180px; width:33%">
 	<input type="hidden" name="action" value="Other">
 	</form>
 
- <form id="Messages_btn" method="post" action="/Logged/go_to_messages" ><button class="btn-success btn-lg" type="submit" name="mail" style="height:180px; width:99.6%" >go to mail</button></form>
+ <form id="Messages_btn" method="post" action="/Logged/go_to_messages" ><button class="btn-primary btn-lg" type="submit" name="mail" style="height:180px; width:99.6%" >go to mail</button></form>
 
  <!-- hidden form needed for navBar -->
   <form id="profile" method='POST' action='/Logged/show_profile'><input type="hidden" name="view_profile" value="<?= $this->session->userdata('loggedin_user')['idusers']?>"></form>
