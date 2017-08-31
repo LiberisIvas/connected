3ac12417d6c5a2e513245a72495b0ed78e35a7be
Mahmoud <m4m205@users.noreply.github.com>
2017-08-31 15:45:23 +0200

    log out working in all pages

--- 1/application/views/Template/navigation.php
+++ 1/application/views/Template/navigation.php
@@ -10,7 +10,7 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="Loggedout">Logout <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href=<?= base_url("Loggedout") ?>>Logout <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#" onclick="document.getElementById('Messages_btn').submit();">Messages</a>
